4649adef2bbcdfb9f28c79a852b33f5aead9458b
Adam <145332225+useradamlee@users.noreply.github.com>
2025-10-22 17:30:44 +0800

    Add various changes to website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,4 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({
-  build: {
-    inlineStylesheets: "auto",
-  },
-  compressHTML: true,
-  vite: {
-    build: {
-      cssCodeSplit: true,
-      minify: "terser",
-      terserOptions: {
-        compress: {
-          drop_console: true,
-        },
-      },
-    },
-  },
-});
+// https://astro.build/config
+export default defineConfig({});
